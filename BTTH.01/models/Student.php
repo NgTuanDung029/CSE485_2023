94c153c6a1603dd56281b8b018df75a09698509b
NgTuanDung029 <rivencyber@gmail.com>
2023-05-11 22:31:45 +0700

    get

--- BTTH.01/models/Student.php
+++ BTTH.01/models/Student.php
@@ -1,13 +1,17 @@
-<?php 
-declare (strict_types = 1);
+<?php
+
+declare(strict_types=1);
+
 namespace Models;
-class Student {
+
+class Student
+{
     public string $full_name;
     public string $class;
     public string $code;
     public string $address;
     public string $phone_num;
-    
+
     public function __construct(string $full_name, string $class, string $code, string $address, string $phone_num)
     {
         $this->full_name = $full_name;
@@ -16,7 +20,4 @@ class Student {
         $this->address = $address;
         $this->phone_num = $phone_num;
     }
-
 }
-
-?>
--- BTTH.01/view/index.php
+++ BTTH.01/view/index.php
@@ -7,55 +7,58 @@ use Models\Student;
 $file = file_get_contents("../database/students-data.csv");
 
 if ($file !== false) {
-    $datas = str_getcsv($file, "\n");
-    $students = new StudentList();
+  $datas = str_getcsv($file, "\n");
+  $students = new StudentList();
 } else {
-    echo "Failed to read the file.";
+  echo "Failed to read the file.";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
-    
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Code</th>
-      <th scope="col">Full Name</th>
-      <th scope="col">Class</th>
-      <th scope="col">Address</th>
-      <th scope="col">Phone Number</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-        foreach ($datas as $data) {
-            echo $data . "<br>";
-            $splitData = explode(";", $data);
-            $student = new Student($splitData[1], $splitData[2], $splitData[0], $splitData[3], $splitData[4]);
-            $students->addStudent($student); 
-    ?>
-    <tr>
-      <th scope="row"><?= $student->code ?></th>
-      <td><?= $student->full_name ?></td>
-      <td><?= $student->class ?></td>
-      <td><?= $student->address ?></td>
-      <td><?= $student->phone_num ?></td>
-    </tr>
-    <?php }
-    ?>
-    
-  </tbody>
-</table>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Code</th>
+        <th scope="col">Full Name</th>
+        <th scope="col">Class</th>
+        <th scope="col">Address</th>
+        <th scope="col">Phone Number</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($datas as $data) {
+        echo $data . "<br>";
+        $splitData = explode(";", $data);
+        $student = new Student($splitData[1], $splitData[2], $splitData[0], $splitData[3], $splitData[4]);
+        $students->addStudent($student);
+      ?>
+        <tr>
+          <th scope="row"><?= $student->code ?></th>
+          <td><?= $student->full_name ?></td>
+          <td><?= $student->class ?></td>
+          <td><?= $student->address ?></td>
+          <td><?= $student->phone_num ?></td>
+        </tr>
+      <?php }
+      ?>
+
+    </tbody>
+  </table>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
