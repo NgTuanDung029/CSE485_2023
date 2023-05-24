07aa18d77aa490d11bea8d4fb43a2f190b19ae98
viduc2811 <viduc28112002@gmail.com>
2023-05-24 22:29:13 +0700

    Add attendance page

--- BTTH.02/views/teacher/attendance.php
+++ BTTH.02/views/teacher/attendance.php
@@ -10,3 +10,46 @@
     }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>Điểm danh sinh viên</h1>
+
+    <div class="student-list">
+        <table>
+            <thead>
+                <tr>
+                    <th>Mã sinh viên</th>
+                    <th>Tên sinh viên</th>
+                    <th>Điểm danh</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>SV001</td>
+                    <td>Nguyễn Văn A</td>
+                    <td><input type="checkbox" name="attendance[]" value="SV001"></td>
+                </tr>
+                <tr>
+                    <td>SV002</td>
+                    <td>Trần Thị B</td>
+                    <td><input type="checkbox" name="attendance[]" value="SV002"></td>
+                </tr>
+                <!-- Thêm các dòng cho các sinh viên khác -->
+            </tbody>
+        </table>
+    </div>
+
+    <div class="submit-btn">
+        <button type="button" onclick="submitAttendance()">Gửi điểm danh</button>
+    </div>
+
+</body>
+</html>
